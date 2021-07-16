cc0f42336abc84c8da68920d54db872b4bc93def
Christian Perez <cperez@nextlab.com.ar>
2021-07-16 15:02:25 -0300

    Se agrega custom hook de useForm

---
+++ src/hooks/useForm.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+export function useFormInput(initialState) {
+  const [value, setValue] = useState(initialState);
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return {
+    input: {
+      value,
+      onChange,
+    },
+    setValue,
+  };
+}
+
+export function useFormContent(initialState) {
+  const [content, setContent] = useState(initialState);
+
+  const setValue = (key, value) => {
+    setContent((prevState) => ({ ...prevState, [key]: value }));
+  };
+
+  const onChange = (e) => {
+    const { name, value, type, checked } = e.target;
+
+    setContent((prevState) => ({
+      ...prevState,
+      [name]: type != 'checkbox' ? value : checked,
+    }));
+  };
+
+  return {
+    content,
+    setContent,
+    setValue,
+    onChange,
+  };
+}
