c24e8aeb856a1376d3ce52d539eeffa8710e17b4
Matias Moreno <matiasmorenog@gmail.com>
2021-07-16 12:35:40 -0300

    qr reader

--- src/components/QrReader.js
+++ src/components/QrReader.js
@@ -49,7 +49,6 @@ export default function QrReader(props) {
       stream = await navigator.mediaDevices.getUserMedia({
         video: {
           deviceId: deviceState.input.value,
-          resizeMode: 'none',
           facingMode: 'environment',
         },
       });
@@ -89,9 +88,11 @@ export default function QrReader(props) {
           </MenuItem>
         ))}
       </TextField>
-      <video autoPlay ref={videoRef} />
-      {/* <input type="file" accept="image/*" capture="environment"></input> */}
-      {/* <canvas ref={photoRef} width="640" height="480" /> */}
+      <video
+        style={{ width: 'inherit', height: 'inherit' }}
+        autoPlay
+        ref={videoRef}
+      />
     </Container>
   );
 }
