b84bb830eca59922c9672b5a1e1894afee98c326
Matias Moreno <matiasmorenog@gmail.com>
2021-08-18 08:40:58 -0700

    wip

--- src/components/CodeReader.js
+++ src/components/CodeReader.js
@@ -10,7 +10,7 @@ import {
   MenuItem,
   Paper,
   TextField,
-  Typography,
+  Typography
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import {
@@ -18,7 +18,7 @@ import {
   CloseCircle,
   CreditCardScan,
   MagnifyScan,
-  QrcodeScan,
+  QrcodeScan
 } from 'mdi-material-ui';
 import PropTypes from 'prop-types';
 import React, { forwardRef, useEffect, useRef } from 'react';
@@ -165,28 +165,8 @@ function CodeReader(
     const classes = useStyles();
     const { content, onChange, setValue } = useFormContent({ scanner: '' }),
       { scanner } = content;
-    // const ref = useRef(null);
 
-    const CodeIcon = (props) => {
-      let icon;
-
-      switch (formats[0]) {
-        case 'qr_code':
-          icon = <QrcodeScan {...props} />;
-          break;
-        case 'code_128':
-          icon = <BarcodeScan {...props} />;
-          break;
-        case 'pdf417':
-          icon = <CreditCardScan {...props} />;
-          break;
-        default:
-          icon = <MagnifyScan {...props} />;
-          break;
-      }
-
-      return icon;
-    };
+    
 
     const handleSubmit = (e) => {
       e.preventDefault();
@@ -215,7 +195,7 @@ function CodeReader(
             alignItems="center"
           >
             <Box my={10} position="relative">
-              <CodeIcon className={classes.codeIcon} />
+              <CodeIcon format={formats[0]} className={classes.codeIcon} />
 
               <CircularProgress size="200px" className={classes.progress} />
             </Box>
@@ -256,3 +236,25 @@ CodeReader.propTypes = {
 };
 
 export default forwardRef(CodeReader);
+
+
+function CodeIcon ({format, ...rest}) {
+  let icon;
+
+  switch (format) {
+    case 'qr_code':
+      icon = <QrcodeScan {...rest} />;
+      break;
+    case 'code_128':
+      icon = <BarcodeScan {...rest} />;
+      break;
+    case 'pdf417':
+      icon = <CreditCardScan {...rest} />;
+      break;
+    default:
+      icon = <MagnifyScan {...rest} />;
+      break;
+  }
+
+  return icon;
+};
