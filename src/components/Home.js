3e247a610b3d757057780601335e8912467edba9
Christian Perez <cperez@nextlab.com.ar>
2021-08-19 10:29:14 -0300

    Se ajusta la pantalla y se quitan warnings

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,21 @@
-import {
-  Box,
-  Button,
-  Grid,
-  makeStyles,
-  Typography,
-  Zoom,
-} from '@material-ui/core';
+import { Box, Button, Grid, makeStyles, Typography } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 const transitionTimer = 500;
 
 const useStyles = makeStyles(({ palette }) => ({
-  root: {
-    height: '100vh',
-  },
   gridHeader: {
     textAlign: 'center',
     color: palette.text.secondary,
-    height: '50%',
+    height: '50vh',
+    backgroundColor: palette.deepPurple[500],
   },
   headerBgColor: {
     backgroundColor: palette.deepPurple[500],
   },
   gridBody: {
     backgroundColor: palette.grey[50],
-    height: '50%',
   },
   img: {
     maxWidth: 90,
@@ -63,155 +53,138 @@ export default function Home({ isAuthenticated }) {
   const classes = useStyles();
 
   return (
-    <Zoom appear in={true} timeout={transitionTimer}>
-      <div div className={classes.root}>
-        <Grid container className={classes.gridHeader}>
-          <Grid
-            item
-            xs={12}
-            className={classes.headerBgColor}
-            container
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Box>
-              <Grid
-                container
-                direction="row"
-                justifyContent="center"
-                alignItems="center"
-              >
-                <img
-                  className={classes.imgLogo}
-                  alt="icon-scan"
-                  src="./assets/logo-app.png"
-                />
-                <Typography
-                  component="h1"
-                  variant="h3"
-                  className={classes.title}
-                >
-                  Nextlab Handheld
-                </Typography>
-              </Grid>
-            </Box>
-            <Box mt={1}>
-              <Typography
-                component="h1"
-                variant="h5"
-                className={classes.subtitle}
-              >
-                Ingrese muestras escaneando tubos
-              </Typography>
-            </Box>
-            <Box clone mt={3}>
-              <form onSubmit={() => {}}>
-                <Button
-                  type="submit"
-                  variant="contained"
-                  className={classes.button}
-                  size="large"
-                >
-                  Comenzar a escanear
-                </Button>
-              </form>
-            </Box>
-          </Grid>
-        </Grid>
+    <>
+      <Grid
+        container
+        className={classes.gridHeader}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
         <Grid
           item
+          direction="column"
           xs={12}
-          className={classes.gridBody}
           container
-          direction="row"
           justifyContent="center"
-          alignItems="flex-start"
+          alignItems="center"
         >
-          <Grid
-            container
-            direction="column"
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="center"
+            alignItems="center"
+            className={classes.headerBgColor}
             xs={12}
-            lg={2}
-            className={classes.gridStep}
           >
-            <Grid
-              container
-              justyContent="center"
-              alignItems="center"
-              direction="column"
-            >
-              <img
-                className={classes.img}
-                alt="icon-scan"
-                src="./assets/icon-scan.png"
-              />
-              <Typography
-                component="h1"
-                variant="h5"
-                className={classes.stepTitle}
-              >
-                Escaneo
-              </Typography>
-            </Grid>
-            <Typography component="h1">
-              Escanee mediante codigo de barras para cargar la informacion del
-              tubo
+            <img
+              className={classes.imgLogo}
+              alt="icon-scan"
+              src="./assets/logo-app.png"
+            />
+            <Typography component="h1" variant="h3" className={classes.title}>
+              Nextlab Handheld
             </Typography>
-          </Grid>
-
-          <Grid direction="column" xs={12} lg={2} className={classes.gridStep}>
-            <Grid
-              container
-              justyContent="center"
-              alignItems="center"
-              direction="column"
+          </Box>
+          <Box mt={1}>
+            <Typography
+              component="h1"
+              variant="h5"
+              className={classes.subtitle}
             >
-              <img
-                className={classes.img}
-                alt="complex"
-                src="./assets/icon-scan.png"
-              />
-              <Typography
-                component="h1"
-                variant="h5"
-                className={classes.stepTitle}
-              >
-                Paciente
-              </Typography>
-            </Grid>
-            <Typography component="h1">
-              Escanee el DNI o cargue los datos del paciente a mano
+              Ingrese muestras escaneando tubos
             </Typography>
-          </Grid>
-          <Grid direction="column" xs={12} lg={2} className={classes.gridStep}>
-            <Grid
-              container
-              justyContent="center"
-              alignItems="center"
-              direction="column"
-            >
-              <img
-                className={classes.img}
-                alt="complex"
-                src="./assets/icon-scan.png"
-              />
-              <Typography
-                component="h1"
-                variant="h5"
-                className={classes.stepTitle}
+          </Box>
+          <Box clone mt={3}>
+            <form onSubmit={() => {}}>
+              <Button
+                type="submit"
+                variant="contained"
+                className={classes.button}
+                size="large"
               >
-                Envio
-              </Typography>
-            </Grid>
-
-            <Typography component="h1">
-              Envie toda la informacion que se cargara automaticamente en el LIS
-            </Typography>
-          </Grid>
+                Comenzar a escanear
+              </Button>
+            </form>
+          </Box>
         </Grid>
-      </div>
-    </Zoom>
+      </Grid>
+      <Grid
+        className={classes.gridBody}
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="flex-start"
+      >
+        <Grid
+          item
+          container
+          direction="column"
+          xs={12}
+          lg={2}
+          className={classes.gridStep}
+        >
+          <Box alignItems="center" flexDirection="column">
+            <img
+              className={classes.img}
+              alt="icon-scan"
+              src="./assets/icon-scan.png"
+            />
+            <Typography
+              component="h1"
+              variant="h5"
+              className={classes.stepTitle}
+            >
+              Escaneo
+            </Typography>
+          </Box>
+          <Typography component="h1">
+            Escanee mediante codigo de barras para cargar la informacion del
+            tubo
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} lg={2} className={classes.gridStep}>
+          <Box alignItems="center" flexDirection="column">
+            <img
+              className={classes.img}
+              alt="complex"
+              src="./assets/icon-scan.png"
+            />
+            <Typography
+              component="h1"
+              variant="h5"
+              className={classes.stepTitle}
+            >
+              Paciente
+            </Typography>
+          </Box>
+          <Typography component="h1">
+            Escanee el DNI o cargue los datos del paciente a mano
+          </Typography>
+        </Grid>
+        <Grid item xs={12} lg={2} className={classes.gridStep}>
+          <Box alignItems="center" flexDirection="column">
+            <img
+              className={classes.img}
+              alt="complex"
+              src="./assets/icon-scan.png"
+            />
+            <Typography
+              component="h1"
+              variant="h5"
+              className={classes.stepTitle}
+            >
+              Envio
+            </Typography>
+          </Box>
+
+          <Typography component="h1">
+            Envie toda la informacion que se cargara automaticamente en el LIS
+          </Typography>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
